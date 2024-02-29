57c4811c33b787c25335d5c0f2bcb31944bb507b
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-02-29 16:15:17 +0600

    added uptade

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
 
-const loadPhone = async (searchText , isShowAll) => {
+const loadPhone = async (searchText='13' , isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
@@ -34,8 +34,8 @@ console.log(isShowAll)
           <h2 class="card-title">${phone.phone_name
             }</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button onclick='handelShowDetail("${phone.slug}");' class="btn btn-primary">Show Details</button>
           </div>
         </div>`
 
@@ -45,8 +45,39 @@ phoneContainer.appendChild(phoneCard)
   
   // hide lpoading spiner
   toggolLoadingSpiner(false);
-
   
+}
+
+// modal show
+const handelShowDetail = async(id) => {
+  // console.log(id)
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json();
+  
+  const phone = data.data;
+
+
+  showPhoneDetails(phone)
+}
+
+
+const showPhoneDetails = (phone) => {
+  console.log(phone)
+
+  const phoneName = document.getElementById("show-details-phone-name")
+  phoneName.innerText = phone.name;
+
+const showDetsilContainer=document.getElementById('show-detail-container')
+  showDetsilContainer.innerHTML = `
+  <img src="${phone.image}" alt=""/>
+  <p><span>Storage:</span>${phone?.mainFeatures?.
+    storage}</p>
+
+`
+
+
+
+  show_detail_modal.showModal()
 }
 
 
@@ -54,6 +85,7 @@ const handelSearch = (isShowAll) => {
   toggolLoadingSpiner(true)
   const searchFild = document.getElementById('search-fila');
   const searchText = searchFild.value;
+
   loadPhone(searchText , isShowAll);
 
 }
@@ -76,4 +108,4 @@ const handelShowAll = () => {
    
  }
 
-// loadPhone()
+loadPhone()
